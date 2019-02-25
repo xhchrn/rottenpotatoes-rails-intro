b208b7479afefc40713df615cbf6e15be39bb007
Xiaohan Chen <chernxh@tamu.edu>
2019-02-25 14:50:11 -0600

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,16 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params[:sort] == "title"
+      @movies = Movie.order("title")
+      @hilite = "title"
+    elsif params[:sort] == "release_date"
+      @movies = Movie.order("release_date")
+      @hilite = "release_date"
+    else
+      @movies = Movie.all
+      @hilite = ""
+    end
   end
 
   def new
